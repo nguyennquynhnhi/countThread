0d6613e5909515fed388823232524fd44db597a5
nguyennquynhnhi <161435648+nguyennquynhnhi@users.noreply.github.com>
2024-03-05 18:02:13 +0700

    Add files via upload

---
+++ countThread/IncrementThread.java
@@ -0,0 +1,20 @@
+package countThread;
+
+public class IncrementThread extends Thread{
+    private Counter counter;
+    private int incrementsPerThread;
+    public IncrementThread(Counter counter, int incrementsPerThread) {
+        this.counter = counter;
+        this.incrementsPerThread = incrementsPerThread;
+    }
+    @Override
+    public void run() {
+        for (int i = 0; i < incrementsPerThread; i++) {
+            try {
+                counter.increment();
+            }catch (InterruptedException e) {
+                throw  new RuntimeException(e);
+            }
+        }
+    }
+}
